6c56eb1c7bbe9a06122336baba27d482f4c7b38d
mgah77 <mauricio.gah@gmail.com>
2025-02-14 16:36:51 -0300

    kd

--- models/stock_forecast.py
+++ models/stock_forecast.py
@@ -6,6 +6,7 @@ class ProductProduct(models.Model):
 
     average_monthly_sale = fields.Float(string='Promedio de Venta Mensual', compute='_compute_average_monthly_sale', store=True)
     estimated_exhaustion_month = fields.Date(string='Mes Aproximado de Agotamiento', compute='_compute_estimated_exhaustion_month', store=True)
+    estimated_exhaustion_month_display = fields.Char(string='Mes de Agotamiento (Formato)', compute='_compute_estimated_exhaustion_month_display', store=True)
 
     @api.depends('stock_move_ids')
     def _compute_average_monthly_sale(self):
@@ -24,7 +25,14 @@ class ProductProduct(models.Model):
             if product.qty_available > 0 and product.average_monthly_sale > 0:
                 months_left = product.qty_available / product.average_monthly_sale
                 estimated_date = datetime.now() + timedelta(days=months_left * 30)
-                product.estimated_exhaustion_month = estimated_date.date()  # Guardar como fecha
+                product.estimated_exhaustion_month = estimated_date.date()
             else:
-                product.estimated_exhaustion_month = False  # Si no hay stock o no hay ventas, dejar el campo vacío
-    
+                product.estimated_exhaustion_month = False
+
+    @api.depends('estimated_exhaustion_month')
+    def _compute_estimated_exhaustion_month_display(self):
+        for product in self:
+            if product.estimated_exhaustion_month:
+                product.estimated_exhaustion_month_display = product.estimated_exhaustion_month.strftime('%B %Y')
+            else:
+                product.estimated_exhaustion_month_display = 'N/A'
