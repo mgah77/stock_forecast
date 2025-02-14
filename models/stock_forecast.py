74c8da2b8915fa9ba4b07954776a6ebe152a8e16
mgah77 <mauricio.gah@gmail.com>
2025-02-14 16:41:23 -0300

    Revert "er"
    This reverts commit 36909a7a72ce71e0c2e07447f03319cf38a5c447.

--- models/stock_forecast.py
+++ models/stock_forecast.py
@@ -5,7 +5,7 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     average_monthly_sale = fields.Float(string='Promedio de Venta Mensual', compute='_compute_average_monthly_sale', store=True)
-    estimated_exhaustion_month = fields.Date(string='Mes Aproximado de Agotamiento', compute='_compute_estimated_exhaustion_month', store=True)
+    estimated_exhaustion_month = fields.Char(string='Mes Aproximado de Agotamiento', compute='_compute_estimated_exhaustion_month', store=True)
 
     @api.depends('stock_move_ids')
     def _compute_average_monthly_sale(self):
@@ -24,7 +24,6 @@ class ProductProduct(models.Model):
             if product.qty_available > 0 and product.average_monthly_sale > 0:
                 months_left = product.qty_available / product.average_monthly_sale
                 estimated_date = datetime.now() + timedelta(days=months_left * 30)
-                product.estimated_exhaustion_month = estimated_date.date()  # Guardar como fecha
+                product.estimated_exhaustion_month = estimated_date.strftime('%B %Y')
             else:
-                product.estimated_exhaustion_month = False  # Si no hay stock o no hay ventas, dejar el campo vacío
-    
+                product.estimated_exhaustion_month = 'N/A'
